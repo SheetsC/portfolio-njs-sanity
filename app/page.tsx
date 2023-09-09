f6abe5b578a73874e568a1cfc8810e872418c272
Connor Sheets <connorsheets32@gmail.com>
2023-09-09 13:49:11 -0500

    hopefully pool looks good on mobil

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ import 'font-awesome/css/font-awesome.min.css';
 export default function Home() {
   const backgroundPoolStyle = {
     backgroundImage: 'url("https://cdn.pixabay.com/photo/2015/11/02/18/32/water-1018808_1280.jpg")',
-    backgroundSize: '100% 100%',  // or '600px 400px'
+    backgroundSize: 'cover',  // or '600px 400px'
     backgroundAttachment: 'fixed',
   };
   return (
