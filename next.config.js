50767434b495aee2325beb454997a0fab3c9493e
Connor Sheets <connorsheets32@gmail.com>
2023-09-10 10:35:52 -0500

    added more style and about

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {images: {
-    domains: ['poolofficemanager.com','www.google.com','cdn.sanity.io' /* ...other allowed domains */],
+    domains: ['poolofficemanager.com','www.google.com','cdn.sanity.io','encrypted-tbn0.gstatic.com' /* ...other allowed domains */],
   }, 
     remotePatterns: [{
       protocal:"https",
