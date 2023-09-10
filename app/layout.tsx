c6984604463134623fd5fe498552c18aaed94cb7
Connor Sheets <connorsheets32@gmail.com>
2023-09-10 07:48:35 -0500

    we have no null reviews

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Head from 'next/head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,6 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Arial&display=swap" rel="stylesheet" />
+      </Head>
       <body className={inter.className}>{children}</body>
     </html>
   )
